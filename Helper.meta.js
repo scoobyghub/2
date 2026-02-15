0de1c8ad0f08d6ac84d65e79c21076d5901cf982
Scooby <tmnsparky@hotmail.co.uk>
2026-02-15 16:07:58 +0000

    Update script metadata for version 13.00 beta

--- Helper.meta.js
+++ Helper.meta.js
@@ -1,18 +1,20 @@
 // ==UserScript==
-// @name         TMN 2010 Automation Helper v12.10
+// @name         TMN 2010 Automation Helper v13.00 beta
 // @namespace    http://tampermonkey.net/
-// @version      12.10
-// @description  v12.10 + Single tab + Flicker fix + UI cleanup + Garage minutes
+// @version      13.00
+// @description  v13.00 beta + Auto OC/DTM accept, mail content in Telegram alerts
 // @author       You
 // @match        *://www.tmn2010.net/login.aspx*
 // @match        *://www.tmn2010.net/authenticated/*
 // @match        *://www.tmn2010.net/Login.aspx*
 // @match        *://www.tmn2010.net/Authenticated/*
+// @match        *://www.tmn2010.net/Default.aspx*
+// @match        *://www.tmn2010.net/default.aspx*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @connect      api.telegram.org
-// @updateURL    https://not.in.use.home.ballz.uk/tmn/autotmn.meta.js
-// @downloadURL  https://not.in.use.home.ballz.uk/tmn/autotmn.user.js
+// @updateURL    https://raw.githubusercontent.com/scoobyghub/2/refs/heads/main/Helper.meta.js
+// @downloadURL  https://raw.githubusercontent.com/scoobyghub/2/refs/heads/main/Helper.user.js
 // ==/UserScript==
